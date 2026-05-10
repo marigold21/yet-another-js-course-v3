4c58389c80318d6e8dc6609ac6713b35c7951f28
Olha Hrinchenko <olgreeen@gmail.com>
2026-05-10 15:59:31 +0200

    Skip login test for valid credentials

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Verify login with valid credentials', async ({ page }) => {
+test.skip('Verify login with valid credentials', async ({ page }) => {
 await page.goto('https://practicesoftwaretesting.com/auth/login');
 await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
 await page.locator('[data-test="password"]').fill('welcome01');
