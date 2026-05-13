7b0507b892857336b2d33ff97ab02999a7648b05
Olha Hrinchenko <139347103+marigold21@users.noreply.github.com>
2026-05-13 17:36:11 +0200

    Update tests/login.spec.ts
    Co-authored-by: Pavlo Hlazkov <50332329+hlazkov@users.noreply.github.com>

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.skip('Verify login with valid credentials', async ({ page }) => {
-await page.goto('https://practicesoftwaretesting.com/auth/login');
+await page.goto('/auth/login');
 await page.getByTestId('email').fill('customer@practicesoftwaretesting.com');
 await page.getByTestId('password').fill('welcome01');
 await page.getByTestId('login-submit').click();
