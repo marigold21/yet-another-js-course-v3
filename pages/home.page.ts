95b6365528d0c969a98094bd01daaff79cd2c51c
Olha Hrinchenko <olgreeen@gmail.com>
2026-05-25 22:33:24 +0200

    refactor: remove unnecessary response wait in openHomePage and enhance clickProductByName method for improved element selection

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -11,15 +11,15 @@ export class HomePage {
   }
 
   async openHomePage() {
-    const responsePromise = this.page.waitForResponse('**/products');
     await this.page.goto('/');
-    await responsePromise;
   }
 
-  async clickProductByName(productName: string) {
-    await this.page
-      .locator('[data-test="product-name"]')
-      .filter({ hasText: new RegExp(productName) })
-      .click();
-  }
+async clickProductByName(productName: string) {
+  const productLocator = this.page
+    .getByTestId('product-name')
+    .filter({ hasText: new RegExp(productName) });
+  
+  await productLocator.first().waitFor({ state: 'visible', timeout: 60000 });
+  await productLocator.first().click();
+}
 }
